38c6aad790b5e39bb7822d4e4ff535ce0affd24d
Tobias Albrecht <tobias.albrecht@albrecht.services>
2022-11-30 13:21:47 +0100

    feat protocol: added authentication transportation hybrid
    extended AuthenticatorTransport type with new hybrid value incl. description from official documentation
    
    Fixes #74

--- protocol/authenticator.go
+++ protocol/authenticator.go
@@ -99,6 +99,8 @@ const (
 	BLE AuthenticatorTransport = "ble"
 	// Internal the client should use an internal source like a TPM or SE
 	Internal AuthenticatorTransport = "internal"
+	// HYBRID indicates the respective authenticator can be contacted using a combination of (often separate) data-transport and proximity mechanisms. This supports, for example, authentication on a desktop computer using a smartphone.
+	HYBRID AuthenticatorTransport = "hybrid"
 )
 
 // A WebAuthn Relying Party may require user verification for some of its operations but not for others,
