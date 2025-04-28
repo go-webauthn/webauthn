64ae734fbbb65fe2890d10eb1f45b1a1268000ad
James Elliott <james-d-elliott@users.noreply.github.com>
2025-04-28 20:27:44 +1000

    feat(protocol): cable transport
    This adds a remapping for the cable transport which has been observed in the wild, even though it has not been ratified and only exited for a short while before it was renamed.

--- protocol/const.go
+++ protocol/const.go
@@ -9,3 +9,13 @@ const (
 	stmtCertInfo  = "certInfo"
 	stmtPubArea   = "pubArea"
 )
+
+var (
+	// internalRemappedAuthenticatorTransport handles remapping of AuthenticatorTransport values. Specifically it is
+	// intentional on remapping only transports that never made recommendation but are being used in the wild. It
+	// should not be used to handle transports that were ratified.
+	internalRemappedAuthenticatorTransport = map[string]AuthenticatorTransport{
+		// The Authenticator Transport 'hybrid' was previously named 'cable'; even if it was for a short period.
+		"cable": Hybrid,
+	}
+)
