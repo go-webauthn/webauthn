a6914b7ac10e4a97ba057dfe7771376b73f136b6
James Elliott <james-d-elliott@users.noreply.github.com>
2026-01-02 21:50:31 +1100

    feat(protocol): extensions

---
+++ protocol/ext_registration_inputs.go
@@ -0,0 +1,89 @@
+package protocol
+
+import "encoding/json"
+
+type AttestationExtensionsClientInputs struct {
+	AppIDExclude        *string            `json:"appidExclude,omitempty"`
+	CredBlob            *URLEncodedBase64  `json:"credBlob,omitempty"`
+	CredProps           *bool              `json:"credProps,omitempty"`
+	CredProtect         *CredProtectInputs `json:"credProtect,omitempty"`
+	HMACCreateSecret    *bool              `json:"hmacCreateSecret,omitempty"`
+	LargeBlob           *LargeBlobInputs   `json:"largeBlob,omitempty"`
+	LargeBlobKey        *bool              `json:"largeBlobKey,omitempty"`
+	MinPinLength        *bool              `json:"minPinLength,omitempty"`
+	PinComplexityPolicy *bool              `json:"pinComplexityPolicy,omitempty"`
+	PRF                 *PRFInputs         `json:"prf,omitempty"`
+	ThirdPartyPayment   *bool              `json:"thirdPartyPayment,omitempty"`
+
+	Extra map[string]json.RawMessage `json:"-"`
+}
+
+func (r *AttestationExtensionsClientInputs) UnmarshalJSON(data []byte) (err error) {
+	type alias AttestationExtensionsClientInputs
+
+	var known alias
+
+	if err = json.Unmarshal(data, &known); err != nil {
+		return err
+	}
+
+	*r = AttestationExtensionsClientInputs(known)
+
+	var m map[string]json.RawMessage
+
+	if err = json.Unmarshal(data, &m); err != nil {
+		return err
+	}
+
+	delete(m, "appidExclude")
+	delete(m, "credProps")
+	delete(m, "credProtect")
+	delete(m, "credBlob")
+	delete(m, "prf")
+	delete(m, "largeBlob")
+	delete(m, "minPinLength")
+	delete(m, "pinComplexityPolicy")
+	delete(m, "hmacCreateSecret")
+	delete(m, "largeBlobKey")
+	delete(m, "thirdPartyPayment")
+
+	if len(m) > 0 {
+		r.Extra = m
+	}
+
+	return nil
+}
+
+func (r AttestationExtensionsClientInputs) MarshalJSON() (data []byte, err error) {
+	type alias AttestationExtensionsClientInputs
+
+	m := map[string]any{}
+
+	if data, err = json.Marshal(alias(r)); err != nil {
+		return nil, err
+	}
+
+	if err = json.Unmarshal(data, &m); err != nil {
+		return nil, err
+	}
+
+	for k, v := range r.Extra {
+		if _, exists := m[k]; !exists {
+			m[k] = v
+		}
+	}
+
+	return json.Marshal(m)
+}
+
+type CredProtectInputs struct {
+	CredentialProtectionPolicy        string `json:"credentialProtectionPolicy,omitempty"`
+	EnforceCredentialProtectionPolicy bool   `json:"enforceCredentialProtectionPolicy,omitempty"`
+}
+
+var (
+	_ json.Marshaler   = (*AttestationExtensionsClientInputs)(nil)
+	_ json.Unmarshaler = (*AttestationExtensionsClientInputs)(nil)
+	_ Marshallable     = (*AttestationExtensionsClientInputs)(nil)
+	_ json.Marshaler   = AttestationExtensionsClientInputs{}
+)
