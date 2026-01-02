220605d09bf6946bb57f53e5398b95749bf2989c
James Elliott <james-d-elliott@users.noreply.github.com>
2026-01-03 01:38:00 +1100

    feat(protocol): signals structs

---
+++ protocol/signals.go
@@ -0,0 +1,40 @@
+package protocol
+
+func NewSignalAllAcceptedCredentials(rpid string, user AllAcceptedCredentialsUser) *SignalAllAcceptedCredentials {
+	credentials := user.WebAuthnCredentialIDs()
+
+	ids := make([]URLEncodedBase64, 0, len(credentials))
+
+	for _, id := range credentials {
+		ids = append(ids, id)
+	}
+
+	return &SignalAllAcceptedCredentials{
+		AllAcceptedCredentialIDs: ids,
+		RPID:                     rpid,
+		UserID:                   user.WebAuthnID(),
+	}
+}
+
+type SignalAllAcceptedCredentials struct {
+	AllAcceptedCredentialIDs []URLEncodedBase64 `json:"allAcceptedCredentialIds"`
+	RPID                     string             `json:"rpId"`
+	UserID                   URLEncodedBase64   `json:"userId"`
+}
+
+type SignalCurrentUserDetails struct {
+	DisplayName string           `json:"displayName"`
+	Name        string           `json:"name"`
+	RPID        string           `json:"rpId"`
+	UserID      URLEncodedBase64 `json:"userId"`
+}
+
+type SignalUnknownCredential struct {
+	CredentialID URLEncodedBase64 `json:"credentialId"`
+	RPID         string           `json:"rpId"`
+}
+
+type AllAcceptedCredentialsUser interface {
+	WebAuthnID() []byte
+	WebAuthnCredentialIDs() [][]byte
+}
